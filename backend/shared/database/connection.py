9599d3288910372c0c7212a66e85ffedf4501764
djabi <djabi@users.noreply.github.com>
2026-03-06 23:31:08 +0100

    suppression des fichiers destkop inutile pour al version mobile

--- backend/shared/database/connection.py
+++
@@ -1,4 +0,0 @@
-# Backward compatibility - redirect to database package
-from shared.database.database import get_db_connection, close_connection
-
-__all__ = ['get_db_connection', 'close_connection']
